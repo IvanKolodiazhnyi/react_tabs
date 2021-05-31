d7371f4ef7e20543ec1f8e7ce93d9d8f29e50357
Ivan Kolodyazhnyi <80517717+IvanKolodiazhnyi@users.noreply.github.com>
2021-05-31 08:58:57 +0300

    Updates

---
+++ precache-manifest.f722f451e6af8ce6d1656194a5f49e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5faaf910ef24068e12528660e5947a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4cd8fe22532a76935bac",
+    "url": "./static/css/main.f065317c.chunk.css"
+  },
+  {
+    "revision": "b5d918b9f242729e23aa",
+    "url": "./static/js/2.e3f75996.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.e3f75996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd8fe22532a76935bac",
+    "url": "./static/js/main.3a49d484.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
